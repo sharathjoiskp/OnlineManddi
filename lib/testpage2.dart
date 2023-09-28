7b5c70ae595f724263d02eed978d2d0c8496329c
sharatjoiskp <sharatjoiskp@gmail.com>
2023-09-29 00:45:14 +0530

    nw file test2 added for git exp

---
+++ lib/testpage2.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TestPage2 extends StatelessWidget {
+  const TestPage2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
